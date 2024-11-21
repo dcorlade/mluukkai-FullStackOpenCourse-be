7fca1afd9b444e88dd7e6f5d3203b39b5ceb8a0b
Denis Corlade <corladedenis19@gmail.com>
2024-11-21 13:38:01 +0200

    3.9

--- index.js
+++ index.js
@@ -94,6 +94,7 @@ app.post('/api/persons/', (request, response) => {
   response.json(person)
 })
 
+// Completed exercise 3.9
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
