4c4a22d40086bafb035497a4ebbd0ed2ad9ea567
Denis Corlade <corladedenis19@gmail.com>
2024-11-25 16:32:16 +0200

    uibuild

--- index.js
+++ index.js
@@ -62,6 +62,21 @@ app.get('/api/persons/:id', (request, response, next) => {
   .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 app.delete('/api/persons/:id', (request, response) => {
   Person.findByIdAndDelete(request.params.id).then(_ => {
     response.status(204).end()
