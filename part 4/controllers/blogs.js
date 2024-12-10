200e0858cb494e7bca3f9ee6e5fcabba06f8637f
Denis Corlade <corladedenis19@gmail.com>
2024-12-10 17:59:42 +0200

    4.17

--- part 4/controllers/blogs.js
+++ part 4/controllers/blogs.js
@@ -1,16 +1,38 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+  const users = await User.find() // Fetch all users
+
+  let user
+
+  if (body.userId) {
+    user = await User.findById(body.userId)
+  } else {
+    // Select a random user if no userId is provided
+    const randomIndex = Math.floor(Math.random() * users.length)
+    user = users[randomIndex] // Select a random user
+  }
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user.id
+  })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 
